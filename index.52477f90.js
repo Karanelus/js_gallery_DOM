7dec89a2fd07932178ecaffe806b5a46852ef50c
Rusłan Karaniewski <karane88vka@gmail.com>
2024-06-20 17:21:13 +0200

    make build

---
+++ index.52477f90.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#largeImg"),r=document.querySelector(".gallery__list");r&&r.addEventListener("click",function(r){r.preventDefault();var t=r.target.closest(".list-item");if(t){var l=t.querySelector("a").href;e&&(e.src=l)}});
+//# sourceMappingURL=index.52477f90.js.map
